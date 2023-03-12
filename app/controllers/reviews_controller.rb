7c8df00b50fe331fa2db140aa47dcda7ac0bdcc1
SM0322 <118785066+SM0322@users.noreply.github.com>
2023-03-12 23:36:38 +0000

    user情報がない?

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,13 @@ class ReviewsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @review = Review.new(reviews_params)
-    @review.save
-    redirect_to book_path(@book)
+    @review.book_id = @book.id
+    @review.user_id = current_user.id
+    if @review.save!
+      redirect_to book_path(@book)
+    else
+      redirect_to books_path
+    end 
   end 
   
   private
